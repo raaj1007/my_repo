befb213dac2774df88317f4726bb3a05efde9b2d
BHAGYA RAAJ <145355499+raaj1007@users.noreply.github.com>
2023-09-21 22:52:36 +0530

    Update visualize.py
    added changes

--- visualize.py
+++ visualize.py
@@ -2,56 +2,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV data into a DataFrame
-df = pd.read_csv("suicide_reates.csv")
+data = pd.read_csv("suicide_reates.csv")
 
-# Menu for user's choice
-print("Select a graph to display:")
-print("1. Bar Graph")
-print("2. Histogram")
-print("3. Scatter Plot")
-print("4. Line Graph")  # Add Line Graph option
+# Create a new DataFrame (data2) without the "INDICATOR" and "UNIT" columns
+data2 = data.drop(columns=["INDICATOR", "UNIT", "FLAG"])
 
-choice = input("Enter your choice (1/2/3/4): ")
+# Count the occurrences of each unique value in the "STUB_LABEL" column
+stub_label_counts = data2["STUB_LABEL"].value_counts()
 
-# Convert the choice to an integer
-choice = int(choice)
+# Extract the "INDICATOR" value to use as the title
+indicator_label = data['INDICATOR'].iloc[0]
 
-# Check the user's choice and create the corresponding plot
-if choice == 1:
-    # Bar Graph
-    plt.figure(figsize=(10, 6))
-    plt.bar(df["YEAR"], df["ESTIMATE"])
-    plt.title("Bar Graph: Suicide Death Rates Over Time")
-    plt.xlabel("Year")
-    plt.ylabel("Death Rate (per 100,000 population)")
-    plt.grid(axis="y")
-    plt.show()
-elif choice == 2:
-    # Histogram
-    plt.figure(figsize=(10, 6))
-    plt.hist(df["ESTIMATE"], bins=5, edgecolor="k")
-    plt.title("Histogram: Suicide Death Rates")
-    plt.xlabel("Death Rate (per 100,000 population)")
-    plt.ylabel("Frequency")
-    plt.grid(axis="y")
-    plt.show()
-elif choice == 3:
-    # Scatter Plot
-    plt.figure(figsize=(10, 6))
-    plt.scatter(df["YEAR"], df["ESTIMATE"], s=50, alpha=0.7)
-    plt.title("Scatter Plot: Suicide Death Rates Over Time")
-    plt.xlabel("Year")
-    plt.ylabel("Death Rate (per 100,000 population)")
-    plt.grid(True)
-    plt.show()
-elif choice == 4:
-    # Line Graph
-    plt.figure(figsize=(10, 6))
-    plt.plot(df["YEAR"], df["ESTIMATE"], marker='o', linestyle='-')
-    plt.title("Line Graph: Suicide Death Rates Over Time")
-    plt.xlabel("Year")
-    plt.ylabel("Death Rate (per 100,000 population)")
-    plt.grid(True)
-    plt.show()
-else:
-    print("Invalid choice. Please select 1, 2, 3, or 4.")
+# Create a 2x2 grid of plots
+plt.figure(figsize=(12, 10))
+
+# Plot 1: Scatter plot (AGE_NUM vs. ESTIMATE)
+plt.subplot(2, 2, 1)
+plt.scatter(data2["AGE_NUM"], data2["ESTIMATE"], marker='o', color='green', alpha=0.5)
+plt.title("Scatter Plot: AGE_NUM vs. ESTIMATE")
+plt.xlabel("AGE_NUM")
+plt.ylabel("ESTIMATE")
+
+# Plot 2: Histogram (AGE_NUM distribution)
+plt.subplot(2, 2, 2)
+plt.hist(data2["AGE_NUM"], bins=10, color='blue', alpha=0.7)
+plt.title("Histogram: AGE_NUM Distribution")
+plt.xlabel("AGE_NUM")
+plt.ylabel("Frequency")
+
+# Plot 3: Bar graph for "STUB_NAME" vs. "STUB_NAME_NUM"
+plt.subplot(2, 2, 3)
+stub_name_data = data2['STUB_NAME']
+stub_name_num_data = data2['STUB_NAME_NUM']
+plt.bar(stub_name_data, stub_name_num_data, color='purple', alpha=0.7)
+plt.title("Bar Graph: STUB_NAME vs. STUB_NAME_NUM")
+plt.xlabel("STUB_NAME")
+plt.ylabel("STUB_NAME_NUM")
+plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
+
+# Plot 4: Line graph (STUB_NAME_NUM vs. YEAR)
+plt.subplot(2, 2, 4)
+plt.plot(data2["YEAR"],data2["STUB_NAME_NUM"], marker='o', linestyle='-')
+plt.title("Line Graph: STUB_NAME_NUM vs. YEAR")
+plt.ylabel("STUB_NAME_NUM")
+plt.xlabel("YEAR")
+
+# Add a title for the entire set of plots
+plt.suptitle(f"Data Visualization for: {indicator_label}", fontsize=16)
+
+# Adjust layout and show the plots
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
